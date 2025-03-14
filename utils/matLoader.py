0711673b69c7a3905a89eba3067b8d3c44058f4b
Luckybird <1342979416@qq.com>
2025-03-14 19:07:13 +0800

    重构matLoader.py，新增load_and_slice_mat_images函数以读取和切分mat文件中的图像

--- utils/matLoader.py
+++ utils/matLoader.py
@@ -1,29 +1,39 @@
 import os
+
 import numpy as np
 import scipy.io as sio
-from matplotlib import pyplot as plt
 
-current_dir = os.getcwd() + "/../data/2015_BOE_Chiu/"
-file_name_array = os.listdir(current_dir)[0:1]
+def load_and_slice_mat_images(directory):
+    """
+    读取指定路径的所有mat文件，并将其切分成单张图像。
+    
+    参数:
+    directory (str): 存放mat文件的路径
+    
+    返回:
+    np.array: 切分后的图像数组
 
-all_images = []
-for file_name in file_name_array:
-    if "mat" in file_name:
-        print("loading {}".format(file_name))
-        mat_data = sio.loadmat(os.path.join(current_dir, file_name))
-        all_images.append(mat_data["images"])
+    使用示例:
+    ```python
+    current_dir = os.getcwd() + "/../data/2015_BOE_Chiu/"
+    sliced_images_np = load_and_slice_mat_images(current_dir)
+    ```
+    """
+    file_name_array = os.listdir(directory)
+    all_images = []
+    
+    for file_name in file_name_array:
+        if file_name.endswith(".mat"):
+            print("loading {}".format(file_name))
+            mat_data = sio.loadmat(os.path.join(directory, file_name))
+            all_images.append(mat_data["images"])
+    
+    sliced_images = []
+    for image in all_images:
+        for i in range(image.shape[2]):
+            sliced_images.append(image[:, :, i])
+    
+    sliced_images_np = np.array(sliced_images)
+    print("sliced images np shape: {}".format(sliced_images_np.shape))
+    return sliced_images_np
 
-print("all images shape: {}".format(all_images[0].shape))
-
-first_image = all_images[0]
-
-# 生成3D网格点，注意顺序调整为 y, x, z
-y, x, z = np.mgrid[0:first_image.shape[0], 0:first_image.shape[1], 0:first_image.shape[2]]
-
-# 提取非零值（避免显示全零区域）
-mask = first_image > 0  # 根据数据调整阈值
-
-fig = plt.figure(figsize=(10, 10))
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(x[mask], y[mask], z[mask], c=first_image[mask], cmap='gray', s=1)
-plt.show()
